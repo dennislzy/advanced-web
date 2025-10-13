ad4b5aabb99e09e29168ed082187f529cafa1b81
dennis <egroup.intern.dennis@gmail.com>
2025-10-13 11:08:28 +0800

    fix

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,20 +1,20 @@
+import { commonContainer, commonItem, commonPrice, commonTitle } from "../style/commonStyle";
 import { productList } from "./productList";
-import { productContainer, productItem, productPrice, productTitle } from "./productStyle";
 
 
-const Product = ()=>{
+const common = ()=>{
     return (
-        <div style={productContainer}>
-            <h1 style={productTitle}>Product Page</h1>
+        <div style={commonContainer}>
+            <h1 style={commonTitle}>common Page</h1>
             <ul>
-                {productList.map(product => (
-                    <li key={product.id} style={productItem}>
-                        <span>{product.name}</span>
-                        <span style={productPrice} >${product.price}</span>
+                {productList.map(common => (
+                    <li key={common.id} style={commonItem}>
+                        <span>{common.name}</span>
+                        <span style={commonPrice} >${common.price}</span>
                     </li>
                 ))}
             </ul>
         </div>
     )
 }
-export default Product;
+export default common;
